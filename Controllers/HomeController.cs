ecaa9293d10e2424e5206ed4f729937a775ed3bc
zsuzitor <avdosevstas@mail.ru>
2018-01-02 18:14:39 +0300

    start list

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,8 +17,30 @@ namespace online_store.Controllers
         {
             return View();
         }
+        public ActionResult List_objects()
+        {
+            List<Object_os_for_view> res = new List<Object_os_for_view>();
 
-       
+
+            return PartialView(res);
+        }
+        public ActionResult Object_view(int id)
+        {
+            return View();
+        }
+
+
+        public ActionResult Object_follow(int id)
+        {
+            ViewBag.Id = id;
+            return PartialView();
+        }
+        public ActionResult Object_add_basket(int id)
+        {
+            ViewBag.Id = id;
+            return PartialView();
+        }
+
 
 
 
@@ -40,7 +62,8 @@ namespace online_store.Controllers
         //-----------------------------------
         public ActionResult Main_header()
         {
-
+            
+            ViewBag.List_class_for_header=new string[] {"what1", "what2", "what3", "what4", "what5", "what6", "what7", "what7" };
 
             return PartialView();
         }
